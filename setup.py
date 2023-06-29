afff34ee4a452080e7f421f00563ea844a98581a
Beatrice Vaienti <45464157+BeatriceVaienti@users.noreply.github.com>
2023-06-29 20:31:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
         'cadquery @ git+https://github.com/CadQuery/cadquery.git@master',
         'ipykernel',
         'pydelatin',
-        'scikit-learn==0.17'
+        'scikit-learn>=1.2'
     ],
 )
